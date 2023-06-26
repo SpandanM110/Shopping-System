29f71ebc427eb5267725c9176e93af1a808df61d
Spandan Mukherjee <95229740+SpandanM110@users.noreply.github.com>
2023-06-27 00:37:41 +0530

    updates

---
+++ shopping system/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Glasses",
+    price: 45,
+    desc: "To make a new glasses for you.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 80,
+    desc: "Office Outfits for a perfect day.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "To have a good fit for every occassions.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 150,
+    desc: "To make it official and work better.",
+    img: "images/img-5.jpg",
+  },
+];
